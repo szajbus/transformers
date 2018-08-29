588a8676a751e30eafd6408c29dba61ffeba86fc
Michał Szajbe <michal.szajbe@gmail.com>
2018-08-29 21:36:42 +0200

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -7,22 +7,41 @@ defmodule Transformers.MixProject do
       version: "0.1.0",
       elixir: "~> 1.3",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      package: package(),
+      deps: deps(),
+      description: description(),
+      name: "Transformers",
+      source_url: "https://github.com/szajbus/transformers",
+      docs: docs()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:ex_doc, "~> 0.18.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description do
+    "Transforms nested maps and list of maps"
+  end
+
+  defp package do
+    [
+      name: "transformers",
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/szajbus/transformers"}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      extras: ["README.md"]
     ]
   end
 end
